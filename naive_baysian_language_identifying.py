f6eb1011eeda2be0d2d94dec7293469c6462b1c8
NedaForoutan <65306639+NedaForoutan@users.noreply.github.com>
2023-10-20 17:44:51 +0200

    Add 3 models

---
+++ naive_baysian_language_identifying.py
@@ -0,0 +1,208 @@
+import csv
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score,confusion_matrix
+
+# Parameters
+n_feat = 200  # number of features
+
+# Read train data
+with open('train.csv', newline='', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    data = []
+    for row in reader:
+      data.append(row)
+train_data = pd.DataFrame(data).sample(frac = 1)   #create the dataframe and shuffle it
+
+# Read valid data
+with open('valid.csv', newline='', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    data = []
+    for row in reader:
+      data.append(row)
+valid_data = pd.DataFrame(data).sample(frac = 1)
+
+# Read test data
+with open('test.csv', newline='', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    data = []
+    for row in reader:
+      data.append(row)
+test_data = pd.DataFrame(data).sample(frac = 1)
+
+print(train_data.head())
+print(train_data.columns.values)
+
+# Get most important bigrams characters
+def get_important_bigram_char(texts,n_feat=200):
+    #output: a list of the N most important bigram characters from a list of sentences
+
+    #fit the bigram model
+    vectorizer = TfidfVectorizer(analyzer='char',
+                                 ngram_range=(2,2),
+                            max_features=n_feat)
+
+    X = vectorizer.fit_transform(texts)
+
+    #Get model feature names
+    feature_names = vectorizer.get_feature_names_out()
+
+    return feature_names
+
+# Obtain bigrams from each language
+features = {}
+features_set = set()
+
+languages = train_data['labels'].unique()
+print(len(languages))
+
+for l in languages:
+
+    #get texts for each language language
+    texts = train_data[train_data.labels==l]['text']
+
+    #get 200 most important bigram chars
+    important_bi_chars = get_important_bigram_char(texts, n_feat)
+
+    #add to dict and set
+    features[l] = important_bi_chars
+    features_set.update(important_bi_chars) #the set of important bigram characters across all languages
+
+
+#create vocabulary list using feature set of important bigram characters
+vocab_dic = dict()
+for i,feature in enumerate(features_set):
+    vocab_dic[feature]=i
+
+# Create feaures for each train sapmle using vocabulary
+vectorizer = TfidfVectorizer(analyzer='char',
+                             ngram_range=(2,2),
+                            max_features= n_feat,
+                            vocabulary=vocab_dic)
+
+# Create featur matrrix for train_texts
+texts = train_data['text']
+X = vectorizer.fit_transform(texts)
+feature_names = vectorizer.get_feature_names_out()
+
+train_feat = pd.DataFrame(data=X.toarray(),columns=feature_names)
+
+#Scale train feature matrix
+train_min = train_feat.min()
+train_max = train_feat.max()
+train_feat = (train_feat - train_min)/(train_max-train_min)
+
+# Add target variable to train_feature matrix
+train_feat['labels'] = list(train_data['labels'])
+
+# Create feature matrix for validation_texts
+texts = valid_data['text']
+X = vectorizer.fit_transform(texts)
+
+valid_feat = pd.DataFrame(data=X.toarray(),columns=feature_names)
+valid_feat = (valid_feat - train_min)/(train_max-train_min)
+valid_feat['labels'] = list(valid_data['labels'])
+
+# Create feature matrix for test_texts
+texts = test_data['text']
+X = vectorizer.fit_transform(texts)
+
+test_feat = pd.DataFrame(data=X.toarray(),columns=feature_names)
+test_feat = (test_feat - train_min)/(train_max-train_min)
+test_feat['labels'] = list(test_data['labels'])
+
+common_feature_matrix = [[0] * len(languages) for _ in range(len(languages))]
+
+# Convert each list of language-features into a set
+sets_of_features = [set(features[l]) for l in languages]
+
+# Calculate the important bigram and fill in the matrix
+for i in range(len(languages)):
+    for j in range(0 , i+1):  # Avoid comparing the same list to itself and avoid duplicate comparisons
+        common_features = sets_of_features[i].intersection(sets_of_features[j])
+        common_feature_count = len(common_features)
+        common_feature_matrix[i][j] = common_feature_count
+        #common_feature_matrix[j][i] = common_feature_count  # Since it's symmetric
+
+# The common_feature_matrix now contains the counts of common bigram between each pair of lists
+
+fig, ax = plt.subplots(figsize=(len(languages), len(languages)))
+
+# Create a heatmap of the common bigram chars matrix
+cax = ax.matshow(common_feature_matrix, cmap="YlGnBu")
+
+# Set the labels for the x and y axes
+ax.set_xticks(np.arange(len(languages)))
+ax.set_yticks(np.arange(len(languages)))
+ax.set_xticklabels(languages)
+ax.set_yticklabels(languages)
+ax.xaxis.set_ticks_position("bottom")
+
+# Add count annotations to the cells
+for i in range(len(languages)):
+    for j in range(0 , i+1):
+        count = common_feature_matrix[i][j]
+        ax.text(j, i, str(count), ha="center", va="center", color="black")
+
+
+# Add a title
+plt.title("Common bigram chars Between Languages")
+
+# Display the plot
+plt.show()
+
+#train Multinomial Naive Bayes Classifier
+model = MultinomialNB()
+train_x = train_feat.drop('labels',axis=1)
+train_y = train_feat['labels']
+model.fit(train_x, train_y)
+
+predict_train = model.predict(train_x)
+train_accuracy = accuracy_score(train_y, predict_train)
+print("train accuracy = ", train_accuracy)
+
+x_test = test_feat.drop('labels', axis=1)
+y_test = test_feat['labels']
+
+#Get predictions on test set
+predict_test = model.predict(x_test)
+test_accuracy = accuracy_score(y_test, predict_test)
+print("test accuracy = ", test_accuracy)
+
+#Create confusion matrix
+conf_matrix = confusion_matrix(y_test, predict_test, labels = languages)
+conf_matrix_df = pd.DataFrame(conf_matrix, columns = languages , index = languages)
+
+#Plot confusion matrix heatmap
+plt.figure(figsize = (10, 10), facecolor = 'w', edgecolor = 'k')
+sns.set(font_scale = 1.5)
+sns.heatmap(conf_matrix_df, cmap = 'coolwarm', annot = True, fmt = '.5g', cbar = False)
+plt.xlabel('Predicted', fontsize = 22)
+plt.ylabel('Actual', fontsize = 22)
+
+#test the prediction of some new texts
+new_text = ["Αυτό θα συμβεί γιατί η αίτηση που έχουν καταθέσει, είτε με τη γνωστή διαδικασία έως τις 25 Σεπτεμβρίου αν είναι νέοι δικαιούχους, είτε αυτόματα από το σύστημα για τους παλιούς, θα παραμένει ακόμα υπό επεξεργασία. Η ΑΑΔΕ προχωρά στη διασταύρωση στοιχείων με βάση τις τελευταίες φορολογικές δηλώσεις. Αν η φορολογική δήλωση δεν έχει εκκαθαριστεί έως τις 25 Σεπτεμβρίου, δεν προχωράει και η διαδικασία πληρωμής για το Market Pass."]
+#true label for new tex is 'el' (modern greek)
+# Create feature matrix for test_texts
+X = vectorizer.fit_transform(new_text)
+new_feat = pd.DataFrame(data=X.toarray(),columns=feature_names)
+#new_feat = (new_feat - train_min)/(train_max-train_min)
+#Get prediction for a text
+predict_new = model.predict(new_feat)
+print(predict_new)
+
+new_text = ["女性は頭や顔などに大けがをして病院に搬送されましたが、命に別状はないということです。体長およそ1メートルと体長およそ50センチのあわせて2頭のクマがいたということです。"]
+#true label for new tex is 'ja' (japanese)
+# Create feature matrix for test_texts
+X = vectorizer.fit_transform(new_text)
+new_feat = pd.DataFrame(data=X.toarray(),columns=feature_names)
+#new_feat = (new_feat - train_min)/(train_max-train_min)
+#Get prediction for a text
+predict_new = model.predict(new_feat)
+print(predict_new)
+
